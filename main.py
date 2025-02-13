9acd67b6a1450a67494fdc7ad4d54d750106d042
DMHarms <dmharms@gmail.com>
2025-02-12 16:00:51 -0800

    cleaned up main.py and player.py; no functionality change

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    
     clock = pygame.time.Clock()
-    dt = 0
     player = Player((SCREEN_WIDTH /2), (SCREEN_HEIGHT / 2))
+    dt = 0
     
     while True:
         for event in pygame.event.get():
