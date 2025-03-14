0861b00398488ca2346973d91840282327ba070d
DMHarms <dmharms@gmail.com>
2025-03-13 17:45:19 -0700

    Added functionality for player to move forward and backward

--- player.py
+++ player.py
@@ -7,6 +7,25 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+    
+    def draw(self, screen):
+        points = self.triangle()
+        pygame.draw.polygon(screen, "white", points, 2)
+        
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+        
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -14,19 +33,7 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    
-    def draw(self, screen):
-        points = self.triangle()
-        pygame.draw.polygon(screen, "white", points, 2)
-        
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
         
-    def update(self, dt):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_a]:
-            self.rotate(-dt)
-            
-        if keys[pygame.K_d]:
-            self.rotate(dt)
