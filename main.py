74e61a3b497f0bc52089b83bd72fb1eb99871216
DMHarms <dmharms@gmail.com>
2025-03-14 07:41:37 -0700

    added code and logic for AsteroidField class; added initialization of it to main; fixed Asteroid update() method to correctly add velocity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
@@ -11,7 +13,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player((SCREEN_WIDTH /2), (SCREEN_HEIGHT / 2))
+    asteroidfield = AsteroidField()
     dt = 0
     
     while True:
