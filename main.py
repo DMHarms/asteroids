062578d989dee02a9d7a0a518837f841049997db
DMHarms <dmharms@gmail.com>
2025-03-13 17:33:51 -0700

    Added functionality for player to rotate left and right

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ def main():
     dt = 0
     
     while True:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
-    
+
 if __name__ == "__main__":
     main()
 
